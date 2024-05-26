5e5a6b258f6155980c1fde83d7c4e04de6f8e329
Max <aamaximon989@gmail.com>
2024-05-25 20:04:11 -0500

    update 4 files and delete 4 files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 const ALL_OPS: OPInfo[] = [];
 const ATTACKER_BUTTON = document.getElementById("attack-op");
 const DEFENDER_BUTTON = document.getElementById("defend-op");
-function main() {}
+async function main() {
+    await SetAllOPS();
+}
+
+async function SetAllOPS() {
+    const testing = await document.hasStorageAccess();
+    console.log(testing);
+}
 
 function randomize(type: RandomizerType) {}
+main();
