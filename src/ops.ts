2b74df8d34158d0e1afb4c9d82c397496d990f0a
Max <aamaximon989@gmail.com>
2024-06-21 00:23:38 -0500

    update ops.ts and toKeep.txt

--- src/ops.ts
+++ src/ops.ts
@@ -1,7 +1,7 @@
 import type {Groups} from "./types/group.js";
 import {img} from "./utils/img.js";
 
-const GROUPS: Groups = {
+export const GROUPS: Groups = {
     Attackers: {
         icon: img(),
         ops: [],
