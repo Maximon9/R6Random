b3bc2f16f0beacd6e79d6e8771315b9a1d21ab42
Max <aamaximon989@gmail.com>
2024-08-02 22:08:11 -0500

    update 7 files

--- dist/utils/html.js
+++ dist/utils/html.js
@@ -1,8 +1,10 @@
 export function groupButtonClicked(key) {
     console.log(sessionStorage.getItem("group"));
+    sessionStorage.removeItem("group");
     sessionStorage.setItem("group", key);
     console.log(sessionStorage.getItem("group"));
     console.log(sessionStorage.getItem("roll"));
+    sessionStorage.removeItem("roll");
     sessionStorage.setItem("roll", "1");
     console.log(sessionStorage.getItem("roll"));
 }
--- src/utils/html.ts
+++ src/utils/html.ts
@@ -1,9 +1,11 @@
 export function groupButtonClicked(key: string) {
     console.log(sessionStorage.getItem("group"));
+    sessionStorage.removeItem("group");
     sessionStorage.setItem("group", key);
     console.log(sessionStorage.getItem("group"));
 
     console.log(sessionStorage.getItem("roll"));
+    sessionStorage.removeItem("roll");
     sessionStorage.setItem("roll", "1");
     console.log(sessionStorage.getItem("roll"));
 }
