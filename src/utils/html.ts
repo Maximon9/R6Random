be1d15b67db1d65ac59f0409ea763d3ed136d736
Max <aamaximon989@gmail.com>
2024-08-02 21:53:04 -0500

    update 7 files

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -1,6 +1,11 @@
 export function groupButtonClicked(key: string) {
+    console.log(sessionStorage.getItem("group"));
     sessionStorage.setItem("group", key);
+    console.log(sessionStorage.getItem("group"));
+
+    console.log(sessionStorage.getItem("roll"));
     sessionStorage.setItem("roll", "1");
+    console.log(sessionStorage.getItem("roll"));
 }
 
 export function changeLink(link: string) {
