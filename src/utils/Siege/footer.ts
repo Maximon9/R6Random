5a29f01469a7ad94dc2475f94e5663d32a03ace4
Max <aamaximon989@gmail.com>
2024-08-03 21:48:13 -0500

    update 8 files

--- src/utils/Siege/footer.ts
+++ src/utils/Siege/footer.ts
@@ -1,7 +1,7 @@
 export function createFooter(parentElement: HTMLElement) {
     const footer = document.createElement("footer");
     footer.innerHTML =
-        "This is an unofficial site and not sponsored by Ubisoft. All images are property of Ubisoft. If you have any problems or find any bugs report the issue";
+        "This is an unofficial site and not sponsored by Ubisoft. All images are property of Ubisoft. If you have any problems or find any bugs report the issue ";
     const here = document.createElement("a");
     here.href = "https://github.com/Maximon9/R6Random/issues";
     here.ariaLabel = "Link to the issue page of this websites github repository";
